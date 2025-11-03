aa59d85e1e25b26eb3ed1eb5ba4cb91f25d3b254
Taikimen52 <5taikiiida@gmail.com>
2025-11-03 15:27:17 +0900

    Done: Home tab rendering function(home.js) import & export between home.js & index.js.

--- src/home.js
+++ src/home.js
@@ -1,9 +1,19 @@
 import Shopinfo from "./shopinfo.js"
 console.log(Shopinfo);
 
+export default homeRendering;
 
-class Home {
-    constructor() {
+// Homeタブのコンテンツ生成、店舗情報はshopinfo.jsから取得する
 
+const content = document.querySelector("#content");
+
+function homeRendering() {
+    for(let k of Object.keys(Shopinfo)) {
+        const el = document.createElement("p");
+        el.setAttribute("class", "info");
+        el.setAttribute("id", `${k}`)
+        el.innerText = Shopinfo[k];
+        content.appendChild(el);
     }
 }
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import "./styles.css";
 
+import homeRendering from "./home.js" ;
+
 console.log("This is a test.")
 
+homeRendering();
